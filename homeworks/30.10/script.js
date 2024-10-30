5914dcfc25b6a3384a7012ccd4542dda6429cfe7
Gulaida <bairamovagulaida@gmail.com>
2024-10-30 22:49:14 +0500

    homework

---
+++ homeworks/30.10/script.js
@@ -0,0 +1,11 @@
+var modelName = "BMW";
+var color = "black";
+var wheelsCount = 4;
+var isOffRoad = "Жолсыз аймақпен жүруге арналмаған";
+var rating = "bmw e39 m5";
+
+console.log("Модель атауы:", modelName);
+console.log("Түсі:", color);
+console.log("Дөңгелек саны:", wheelsCount);
+console.log("Жолсыз аймақпен (бездорожье) жүруге арналған ба?", isOffRoad);
+console.log("Рейтингі:", rating);
