6c900f033c1f06ae77890860cd5ed3b53aed5651
Gulaida <bairamovagulaida@gmail.com>
2024-11-03 21:38:12 +0500

    өзгеріс

---
+++ homeworks/31.10/script.js
@@ -0,0 +1,14 @@
+var a = 8; //  slicesPerPizza ;
+var b = 5; // numberOfPizza ;
+
+console.log(a * b); //totalSlices ;// 40;
+
+var a = 24; //totalChocolates;
+var b = 6; // numberOfFriends ;
+
+console.log(a / b); //chocolatesPerFriend ;//4;
+
+let c = 10;
+c += 2; //c=c+5;
+
+console.log(c); //12;
